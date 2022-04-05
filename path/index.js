ff085107c0ec8eeb169f9f9078aeb274a0762ab8
JakariaJishan <jakariajishan987@gmail.com>
2022-04-06 04:05:33 +0600

    new

--- path/index.js
+++ path/index.js
@@ -3,4 +3,4 @@ const path = require('path');
 const x = path.dirname('D:/a_programming/node,express,mongo-thapa/path');
 console.log(x);
 
-console.log(path.parse('D:/a_programming/node,express,mongo-thapa/path/index.js')); //awesome😊
+console.log(path.parse('D:/a_programming/node,express,mongo-thapa/path/index.js')); //awesome😊🤦‍♂️
